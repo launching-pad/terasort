c659d81e78ac63391350c83a4e4ebaf7a40f2c19
ashansa <ashansa.perera@gmail.com>
2015-11-25 10:54:23 +0100

    getting jar from s3

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -6,6 +6,8 @@ script 'run_experiment' do
   interpreter "bash"
   code <<-EOM
 echo "hello"
+mkdir /srv/teragen_jar
+wget https://s3-eu-west-1.amazonaws.com/karamel/terasort_2.10-0.0.1.jar
   EOM
 end
 
