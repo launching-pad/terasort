0eefebf57372e27f5ec73e24d713f8a5d86f1db7
ashansa <ashansa.perera@gmail.com>
2015-11-25 11:17:51 +0100

    running terasort though chef- bug fix

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -8,7 +8,7 @@ script 'run_experiment' do
 echo "hello"
 mkdir /srv/teragen_jar
 wget https://s3-eu-west-1.amazonaws.com/karamel/terasort_2.10-0.0.1.jar
-/usr/local/flink-0.9.1/bin run -p 1 -m 172.28.128.5:6123 -c eastcircle.terasort.FlinkTeraSort /tmp/terasort_2.10-0.0.1.jar hdfs://172.28.128.5:8020 /srv/teragen/ /terasortOut 1 true true
+/usr/local/flink-0.9.1/bin/flink run -p 1 -m 172.28.128.5:6123 -c eastcircle.terasort.FlinkTeraSort /tmp/terasort_2.10-0.0.1.jar hdfs://172.28.128.5:8020 /srv/teragen/ /terasortOut 1 true true
   EOM
 end
 
