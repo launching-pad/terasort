ccab80d387a7486fd755b21365db58e280309db2
Ashansa Perera <ashansa.perera@gmail.com>
2015-12-13 10:55:22 +0100

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -7,7 +7,7 @@ script 'run_experiment' do
   code <<-EOM
 echo "hello"
 wget https://s3-eu-west-1.amazonaws.com/karamel/terasort_2.10-0.0.1.jar
-/usr/local/flink/bin/flink run -p #{node[:flink][:partitions]} -m #{node.terasort.experiment.private_ips[0]}:6123 -c eastcircle.terasort.FlinkTeraSort /tmp/terasort_2.10-0.0.1.jar hdfs://#{node.terasort.experiment.private_ips[0]}:8020 /srv/teragen/ /terasortOut #{node[:flink][:partitions]} true true
+/usr/local/flink/bin/flink run -p #{node[:flink][:partitions]} -m #{node.terasort.experiment.private_ips[0]}:6123 -c eastcircle.terasort.FlinkTeraSort /tmp/terasort_2.10-0.0.1.jar hdfs://#{node.terasort.experiment.private_ips[0]}:8020 /srv/teragen/ /terasortFlink #{node[:flink][:partitions]} true true
   EOM
 end
 
